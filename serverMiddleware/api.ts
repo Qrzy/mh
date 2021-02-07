3a62cab05b05c8b723a7b41956fb1ef3c3176864
Qrzy <qrzy88@gmail.com>
2021-02-07 22:10:33 +0100

    refactor: make server middleware single file

--- serverMiddleware/api.ts
+++ serverMiddleware/api.ts
@@ -1,6 +1,57 @@
+const { graphql } = require('@octokit/graphql');
 const bodyParser = require('body-parser');
 const express = require('express');
-const getMhGithubRepo = require('./getMhGithubRepo');
+
+const query = `
+query RepoFiles($owner: String!, $name: String!) {
+  repository(owner: $owner, name: $name) {
+    object(expression: "master:") {
+      ... on Tree {
+        entries {
+          name
+          type
+          #mode
+          object {
+            ... on Blob {
+              byteSize
+              #text
+              # isBinary
+            }
+            ... on Tree {
+              entries {
+                name
+                type
+                object {
+                  ... on Blob {
+                    byteSize
+                    text
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+`;
+
+const gqlWithAuth = graphql.defaults({
+  headers: {
+    authorization: `token ${process.env.GH_TOKEN}`,
+  },
+});
+
+const getMhGithubRepo = async (mhNumber: string) => {
+  const { repository } = await gqlWithAuth(query, {
+    owner: 'polskimathandel',
+    name: 'PolskiMathandel.github.io',
+  });
+  return repository.object.entries
+    .find((entry: any) => entry.name === `Polski MatHandel #${mhNumber}`)
+    ?.object.entries.filter((entry: any) => (entry.name as string).endsWith('.txt'));
+};
 
 const app = express();
 app.use(bodyParser.json());
--- serverMiddleware/gqlWithAuth.ts
+++
@@ -1,7 +0,0 @@
-const { graphql } = require('@octokit/graphql');
-
-module.exports = graphql.defaults({
-  headers: {
-    authorization: `token ${process.env.GH_TOKEN}`,
-  },
-});
